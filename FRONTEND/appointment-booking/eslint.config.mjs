e84f3f4f5770dc1e80eb90dc70c22b1c39b34121
slaiba123 <139244200+slaiba123@users.noreply.github.com>
2026-01-05 00:15:01 +0500

    changes

--- FRONTEND/appointment-booking/eslint.config.mjs
+++ FRONTEND/appointment-booking/eslint.config.mjs
@@ -1,6 +1,36 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [
+//       "node_modules/**",
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+//   {
+//     "extends": "next/core-web-vitals"
+//   }
+// ];
+
+// export default eslintConfig;
+
+
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +39,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript"
+  ),
   {
     ignores: [
       "node_modules/**",
@@ -20,9 +53,4 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-  {
-    "extends": "next/core-web-vitals"
-  }
 ];
-
-export default eslintConfig;
